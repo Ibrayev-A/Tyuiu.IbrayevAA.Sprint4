e93959019f8f4a90bada5944b710b5ed916a248e
Tyuiu.IbrayevAA@outlook.com <03.05.05.ibraev.abai@gmail.com>
2023-11-12 18:59:38 +0500

    Task5

---
+++ Tyuiu.IbrayevAA.Sprint4.Task5.V25.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IbrayevAA.Sprint4.Task5.V25.Lib;
+
+namespace Tyuiu.IbrayevAA.Sprint4.Task5.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] {  { 0, 2, -2, 1, 3 },
+
+                                           { 0, 2, -2, 1, 3 },
+
+                                           { 0, 2, -2, 1, 3 },
+
+                                           { 0, 2, -2, 1, 3 },
+
+                                           { 0, 2, -2, 1, 3 } };
+            int res = ds.Calculate(mas2);
+            int wait = 30;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IbrayevAA.Sprint4.Task5.V25/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IbrayevAA.Sprint4.Task5.V25.Lib;
+
+namespace Tyuiu.IbrayevAA.Sprint4.Task5.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+            Console.Title = "Спринт #4 | Выполнил: Ибраев А. А. | ПКТБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Ибраев Абай Аскарович | ПКТБ-23-2                             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* значениями с клавиатуры в диапазоне от 3 до 9.                          *");
+            Console.WriteLine("* Заменить четные элементы массива на 1.                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    mtrx[i, j] = rnd.Next(-4, 4);
+                }
+            }
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Сумма положительных элементов = " + res);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
